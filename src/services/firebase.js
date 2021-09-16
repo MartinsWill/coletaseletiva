e85911c95eeb5d354342c1fb20c9ae6097757c1f
WillM <willmartinssdc@hotmail.com>
2021-09-16 14:22:06 -0300

    Corrigindo Erro Firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,8 +4,8 @@ import "firebase/compat/database";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJETC_ID,
+  databaseURL: "https://coleta-seletiva-23ce5-default-rtdb.firebaseio.com",
+  projectId: "coleta-seletiva-23ce5",
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
