28978d08c29de95ff21704533d0e2f7766b89742
WillM <willmartinssdc@hotmail.com>
2021-09-16 14:33:59 -0300

    Erro corrigido Firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: "https://coleta-seletiva-23ce5-default-rtdb.firebaseio.com",
-  projectId: "coleta-seletiva-23ce5",
+  projectId: process.env.REACT_APP_PROJETC_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
